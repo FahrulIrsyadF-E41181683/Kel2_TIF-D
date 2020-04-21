4c05cd76a7f91a34811730af97f5517e53cbc6e3
Fahrul Irsyad Fianto <variationfianto@gmail.com>
2020-04-21 11:45:02 +0700

    Error input data profil done

--- BPBD_JEMBER_WEB/application/views/admin/profil_v.php
+++ BPBD_JEMBER_WEB/application/views/admin/profil_v.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <!-- HEAD MEMANGGIL HEAD YANG ADA DI admin/includes/head.php -->
-    <?php $this->load->view("admin/includes/head.php") ?>
-</head>
-
-<body class="">
-    <!-- SIDEBAR MEMANGGIL SIDEBAR YANG ADA DI admin/includes/sidebar.php -->
-    <?php $this->load->view("admin/includes/sidebar.php") ?>
-
-    <!-- SIDEBAR MEMANGGIL NAVBAR YANG ADA DI admin/includes/sidebar.php -->
-    <?php $this->load->view("admin/includes/navbar.php") ?>
-
     <!-- ISI KONTEN HALAMAN -->
     <?php 
     
@@ -69,19 +54,19 @@
                         <div class="col-md-7 pl-1">
                         <div class="form-group">
                             <label for="email">Email</label>
-                            <input type="email" class="form-control" id="email" name="EMAIL" value="<?= $ad['EMAIL'];?>">
+                            <input type="email" class="form-control" id="EMAIL" name="EMAIL" value="<?= $ad['EMAIL'];?>">
                         </div>
                         </div>
                     </div>
                     <div class="form-group">
                         <label for="nama">Nama</label>
-                        <input type="text" class="form-control" id="nama" name="NAMA" placeholder="Nama" value="<?= $ad['NAMA'];?>">
+                        <input type="text" class="form-control" id="NAMA" name="NAMA" placeholder="Nama" value="<?= $ad['NAMA'];?>">
                     </div>
                     <div class="row">
                         <div class="col-md-12">
                         <div class="form-group">
                             <label>Nomer</label>
-                            <input type="text" class="form-control" id="nomer" name="NOMER" placeholder="Nomer" value="<?= $ad['NOMER'];?>">
+                            <input type="text" class="form-control" id="NOMER" name="NOMER" placeholder="Nomer" value="<?= $ad['NOMER'];?>">
                         </div>
                         </div>
                     </div>
@@ -89,12 +74,12 @@
                         <div class="col-md-12">
                         <div class="form-group">
                             <label>Alamat</label>
-                            <textarea class="form-control textarea" name="ALAMAT"><?= $ad['ALAMAT'];?></textarea>
+                            <textarea class="form-control textarea" id = "ALAMAT" name="ALAMAT"><?= $ad['ALAMAT'];?></textarea>
                         </div>
                         </div>
                     </div>
                     <div class="custom-file">
-                        <input type="file" class="custom-file-input" name="GAMBAR" id="gambar">
+                        <input type="file" class="custom-file-input" name="GAMBAR" id="">
                         <label class="custom-file-label" for="customFile">Choose file</label>
                     </div>
                     <div class="row">
@@ -111,11 +96,76 @@
     <?php endforeach; ?>
     <!-- BATAS ISI KONTEN HALAMAN -->
 
-    <!-- FOOTER MEMANGGIL FOOTER YANG ADA DI admin/includes/footer.php -->
-    <?php $this->load->view("admin/includes/footer.php") ?>
-
-    <!-- JS MEMANGGIL JS YANG ADA DI admin/includes/js.php -->
-    <?php $this->load->view("admin/includes/js.php") ?>
-</body>
-
-</html>
+    <?php
+foreach ($koleksi as $i) :
+    $id_koleksi = $i['id_koleksi'];
+    $judul = $i['judul'];
+    $nim = $i['nim'];
+    $isbn = $i['isbn'];
+    $penerbit = $i['penerbit'];
+    $penulis = $i['penulis'];
+    $tahun_terbit = $i['tahun_terbit'];
+    $nama_kategori = $i['nama_kategori'];
+    ?>
+    <div class="modal fade" id="modal_edit<?= $id_koleksi; ?>" tabindex="-1" role="dialog" aria-labelledby="largeModal" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h3 class="modal-title" id="myModalLabel">Edit Koleksi</h3>
+                    <button class="close" type="button" data-dismiss="modal" aria-hidden="true">x</button>
+                </div>
+                <?= form_open_multipart('koleksi/edit_koleksi'); ?>
+                    <div class="form-group">
+                        <label class="control-label col-xs-3">Judul</label>
+                        <div class="col-xs-8">
+                            <input type="text" class="form-control" name="judul" value="<?= $judul; ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="control-label col-xs-3">NIM</label>
+                        <div class="col-xs-8">
+                            <input type="text" class="form-control" name="nim" value="<?= $nim; ?>" readonly>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-xs-3">ISBN</label>
+                        <div class="col-xs-8">
+                            <input type="text" class="form-control" name="isbn" value="<?= $isbn; ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-xs-3">Penerbit</label>
+                        <div class="col-xs-8">
+                            <input type="text" class="form-control" name="penerbit" value="<?= $penerbit; ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-xs-3">Penulis</label>
+                        <div class="col-xs-8">
+                            <input type="text" class="form-control" name="penulis" value="<?= $penulis; ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="control-label col-xs-3">Tahun Terbit</label>
+                        <div class="col-xs-8">
+                            <input type="text" class="form-control" name="tahun_terbit" value="<?= $tahun_terbit; ?>">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="control-label  col-xs-3">Kategori Koleksi</label>
+                        <select class="form-control" name="nama_kategori" id="nama_kategori">
+                            <?php foreach ($getKategoriKoleksi as $bc) : ?>
+                                <option value="<?= $bc['nama_kategori']; ?>"><?= $bc['nama_kategori']; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+                    <div class="modal-footer">
+                        <button class="btn" data-dismiss="modal" aria-hidden="true">Tutup</button>
+                        <button class="btn btn-info">Update</button>
+                    </div>
+                </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <?php endforeach; ?>
