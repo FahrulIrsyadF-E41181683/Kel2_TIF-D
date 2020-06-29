1ed22c174bffc7e41a2955fab58efab22cf1c346
Rendy Wisnu <rendyrahardjo21@gmail.com>
2020-06-29 20:35:20 +0700

    fix sesuatu yang nggak sinkron di java

--- BPBD_Jember_Android/app/src/main/java/com/bpbd/www/bpbdjember/ProfilActivity.java
+++ BPBD_Jember_Android/app/src/main/java/com/bpbd/www/bpbdjember/ProfilActivity.java
@@ -1,9 +1,8 @@
-package com.example.bpbdjember;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.bpbd.www.bpbdjember;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
